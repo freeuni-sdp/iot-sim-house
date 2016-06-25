c53d116df71a2cab14a3f97807b2a672905b65d2
Nika Chkhartishvili <chxartishvilin@gmail.com>
2016-06-25 15:59:10 +0400

    Add ping service

---
+++ src/main/java/ge/edu/freeuni/sdp/iot/simulators/house/Ping.java
@@ -0,0 +1,19 @@
+package ge.edu.freeuni.sdp.iot.simulators.house;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+/**
+ * Created by nika on 6/25/16.
+ */
+
+@Path("ping")
+@Consumes( { MediaType.APPLICATION_JSON})
+@Produces( { MediaType.APPLICATION_JSON})
+public class Ping {
+
+    @GET
+    public Response get() {
+        return Response.ok().build();
+    }
+
+}
---
+++ src/test/ge/edu/freeuni/sdp/iot/simulators/house/PingTest.java
@@ -0,0 +1,28 @@
+package ge.edu.freeuni.sdp.iot.simulators.house;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nika on 6/25/16.
+ */
+public class PingTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(Ping.class);
+    }
+
+    @Test
+    public void testGetPing() {
+        Response actual = target("ping").request().get();
+        assertEquals(Response.Status.OK.getStatusCode(), actual.getStatus());
+    }
+
+}
