5e64f6a8f7e65a41fdf326d9defc2da8432a59fe
piopao <ekikn12@freeuni.edu.ge>
2016-07-11 00:35:02 +0400

    Conditioner Simulator Implementation

--- src/main/java/ge/edu/freeuni/sdp/iot/simulators/house/model/House.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/simulators/house/model/House.java
@@ -11,11 +11,13 @@ public class House {
     private String houseId;
     private Map<String, Floor> floorMap;
     private Map<String, MacObject> macMap;
+    private String conditionerCondition;
 
     public House(String houseId) {
         this.houseId = houseId;
         floorMap = new HashMap<>();
         macMap = new HashMap<>();
+        conditionerCondition = "0";
     }
 
     public House(String houseId, Map<String, Floor> floorMap) {
@@ -39,7 +41,13 @@ public class House {
         return null;
     }
 
+    public String getConditionerCondition(){
+        return conditionerCondition;
+    }
 
+    public void setConditionerCondition(String condition){
+        this.conditionerCondition = condition;
+    }
 
     public Iterable<Floor> getAllFloors() {
         return new ArrayList<>(floorMap.values());
--- src/main/java/ge/edu/freeuni/sdp/iot/simulators/house/worker/ConditionerSimulator.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/simulators/house/worker/ConditionerSimulator.java
@@ -1,11 +1,19 @@
 package ge.edu.freeuni.sdp.iot.simulators.house.worker;
 
 
+import ge.edu.freeuni.sdp.iot.simulators.house.core.Repository;
+import ge.edu.freeuni.sdp.iot.simulators.house.core.RepositoryFactory;
+import ge.edu.freeuni.sdp.iot.simulators.house.model.Floor;
+import ge.edu.freeuni.sdp.iot.simulators.house.model.House;
+
 /**
  * Created by elene on 6/27/16.
  */
 public class ConditionerSimulator extends Thread {
-    private static final int INTERVAL = 50;
+    private static final int SLEEP_INTERVAL = 50;
+    private static final double MIN_TEMPERATURE = 10.0;
+    private static final double WEAK_STREAM_RATE = 0.0215;
+    private static final double STRONG_STREAM_RATE = 0.042;
 
     private String houseId, condition;
     public ConditionerSimulator(String houseId, String condition){
@@ -15,9 +23,29 @@ public class ConditionerSimulator extends Thread {
 
 
     public void run() {
-        while (true) {
-            //turn temperature down
-            //
+        Repository repository = RepositoryFactory.inMemoryRepositoryInstance();
+
+        while (!interrupted()) {
+            House h = repository.findHouse(this.houseId);
+            for (Floor f : h.getAllFloors()) {
+                double temp = f.getTemperature();
+                switch(h.getConditionerCondition()){
+                    case "0": break;
+                    case "#": break;
+                    case "*":
+                        f.setTemperature((temp<MIN_TEMPERATURE ? temp+(MIN_TEMPERATURE-temp)*WEAK_STREAM_RATE : temp));
+                        break;
+                    case "**":
+                        f.setTemperature((temp<MIN_TEMPERATURE ? temp+(MIN_TEMPERATURE-temp)*STRONG_STREAM_RATE : temp));
+                        break;
+                    default: break;
+                }
+            }
+            try {
+                Thread.sleep(SLEEP_INTERVAL);
+            } catch (InterruptedException e) {
+                break;
+            }
         }
     }
 }
